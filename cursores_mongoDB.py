5b5318a9feab772a0a5ecf0bf1b96c407393dd81
Javier <javiihv93@gmail.com>
2017-05-18 18:10:22 +0200

    remove, update, insert con cursores

--- cursores_mongoDB.py
+++ cursores_mongoDB.py
@@ -2,13 +2,40 @@
 from pymongo import MongoClient
 
 
-# Conexión con la base de datos
+# Conexión con la base de datos llamada "liga" y con la coleccion "premier"
 client = MongoClient()
 db = client.liga
 premierCollection = db.premier
+santanderCollection = db.santander
 
-
+#creación del cursor
 cursor = premierCollection.find({"equipo": "Chelsea"})
+cursor2 = santanderCollection.find({"equipo":"Real Madrid"})
 
-for c in cursor:
-    print c['jugador']
+#Borra los centrocampistas del Chelsea
+def getRemoveMED():
+    for c in cursor:
+        if c['posicion'] == "MED":
+            premierCollection.remove(c)
+
+#actualizar la edad
+def setUpdateAge():
+    for c in cursor2:
+        if c['edad'] == "25":
+            santanderCollection.update_many(c,{"$set": {"edad":31}})
+
+#insertar nuevo jugador
+def getInsertNewPlayer():
+    santanderCollection.insert({
+        "jugador" : 'A.Gallego',
+        "edad" : '25',
+        "estatura": '1.80',
+        "equipo" : 'Real Madrid',
+        "posicion" : 'DEL'
+    })
+
+if __name__ == '__main__':
+   #getRemoveMED()
+   #setUpdateAge()
+   getInsertNewPlayer()
+
